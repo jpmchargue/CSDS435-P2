f4c75df8f69317827992b41366d915e69c5700a8
Aaron3154518 <aaoren210@gmail.com>
2023-04-01 18:56:36 -0400

    Added distance distribution visualization

--- util.py
+++ util.py
@@ -1,20 +1,35 @@
 import matplotlib.pyplot as plt
 import umap
 from sklearn.preprocessing import StandardScaler
+import numpy as np
+
 
 def plot_umap(bow, labels):
-    color_ref = ["slategray", 'r', "orange", 'y', 'g', "aqua", 'b', "indigo", 'm', "black"]
+    color_ref = [
+        "slategray",
+        "r",
+        "orange",
+        "y",
+        "g",
+        "aqua",
+        "b",
+        "indigo",
+        "m",
+        "black",
+    ]
     colors = [color_ref[l] for l in labels]
 
     scaled = StandardScaler().fit_transform(bow)
     umapped = umap.UMAP().fit_transform(scaled)
 
     print("Plotting...")
-    plt.scatter(
-        umapped[:, 0],
-        umapped[:, 1],
-        c=colors
-    )
-    plt.gca().set_aspect('equal', 'datalim')
-    plt.title('UMAP Projection')
+    plt.scatter(umapped[:, 0], umapped[:, 1], c=colors)
+    plt.gca().set_aspect("equal", "datalim")
+    plt.title("UMAP Projection")
     plt.show()
+
+
+def plot_dists(ax, dist_matrix):
+    ax.hist(dist_matrix[np.triu_indices(len(dist_matrix), k=1)], bins=100)
+    ax.set_xlabel("Value")
+    ax.set_ylabel("Count")
