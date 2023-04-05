e1bb4c5ebdb653984cea9f049b061d81f2ee3cc5
James McHargue <jpm221@case.edu>
2023-04-04 22:43:25 -0400

    Added most common words function.

--- distance.py
+++ distance.py
@@ -96,6 +96,32 @@ class Parser():
         print(f"Avg terms per document: {round(self.num_terms_total/self.num_tweets, 2)}")
 
         return vectors
+
+    def get_most_common_words(self, tweet_indices: np.ndarray, num_best_words: int=10) -> List[str]:
+        """
+        Get the most common non-stop words in a cluster.
+
+        Args:
+            tweet_indices (_type_): a numpy array of the indices of tweets in a cluster.
+            num_best_words (int, optional): the number of most common words to get. Defaults to 10.
+
+        Returns:
+            List[str]: a list of the most common words in that cluster.
+        """
+        stop_words = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
+        bag_of_words = self.get_bag_of_words("cnnhealth.txt")
+        tweets = bag_of_words[tweet_indices]
+        word_counts = np.sum(tweets, axis=0)
+        indices = np.argsort(-word_counts)
+        most_common_words = []
+        for i in indices:
+            word = self.int_to_word[i]
+            if word not in stop_words:
+                most_common_words.append(word)
+                if len(most_common_words) >= num_best_words:
+                    return most_common_words
+        return most_common_words
+
         
     def get_distance_matrix(self, filename: str, distance_function: Callable, save_to: str="") -> np.ndarray:
         """
